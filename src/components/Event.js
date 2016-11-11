a64c9f54f280bde9792fa16479eabee72803aac9
Jacob Arriola <jacob@zeek.com>
2016-11-11 11:06:06 -0800

    Remove links in repo

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,11 +4,11 @@ import moment from 'moment';
 class Event extends React.Component {
 	render() {
 		const {type, created_at, repo} = this.props.item;
-		const url = `https://github.com.com/${repo.name}`;
+		// const url = `https://github.com.com/${repo.name}`;
 
 		return (
 			<li className="event-card">
-				<h2>Repo: <a href={url}>{repo.name}</a></h2>
+				<h2>Repo: {repo.name}</h2>
 				<p>{type} - {moment.utc( created_at, "YYYYMMDD" ).fromNow()}</p>
 			</li>
 		)
